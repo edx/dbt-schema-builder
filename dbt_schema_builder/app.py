bec6b83e56cf24eaca5f57f94e183e53d7729416
Purna Shah <120611704+purna2U@users.noreply.github.com>
2023-05-12 16:50:25 -0700

    chore: corrections

--- dbt_schema_builder/app.py
+++ dbt_schema_builder/app.py
@@ -130,31 +130,27 @@ class App:
                         }
                     )
 
-    def update_trifecta_models(self, relation):
+    def update_trifecta_models(self, relation, no_pii=False):
         """
         Given a relation, add it to the 'trifecta'. These are the PII and safe views
         constructed from the raw data.
         """
-        for relation_name in [
-            relation.new_pii_relation_name,
-            relation.new_safe_relation_name,
-        ]:
-            self.add_model_to_new_schema(
-                relation_name, relation.meta_data
-            )
-            
-    def update_trifecta_models_no_pii(self, relation):
-        """
-        Given a relation, add it to the 'trifecta'. These are the PII and safe views
-        constructed from the raw data.
-        """
-        for relation_name in [
-            #relation.new_pii_relation_name,
-            relation.new_safe_relation_name,
-        ]:
-            self.add_model_to_new_schema(
-                relation_name, relation.meta_data
-            )
+        if no_pii is True:
+            for relation_name in [
+                #relation.new_pii_relation_name,
+                relation.new_safe_relation_name,
+            ]:
+                self.add_model_to_new_schema(
+                    relation_name, relation.meta_data
+                )
+        else:
+            for relation_name in [
+                relation.new_pii_relation_name,
+                relation.new_safe_relation_name,
+            ]:
+                self.add_model_to_new_schema(
+                    relation_name, relation.meta_data
+                )
 
     def add_model_to_new_schema(self, new_relation_name, model_meta_data):
         """
