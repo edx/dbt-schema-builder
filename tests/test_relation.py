af099728e64b6c038bf5a544d42deb423d49d292
Purna Shah <120611704+purna2U@users.noreply.github.com>
2023-06-29 16:24:01 -0700

    chore: expand tests

--- tests/test_relation.py
+++ tests/test_relation.py
@@ -288,7 +288,8 @@ def test_sql_soft_delete_no_pii_no_redactions():
     assert 'SCHEMA_NAME' in sql
     assert 'WHERE SOFT_DELETE_COLUMN IS NULL' in sql
 
-def test_add_prefix_to_model_alias():
+
+def test_add_prefix_to_model_alias_with_snowflake_keyword_collision():
     relation = Relation(
         'START',
         ['COLUMN_1', 'COLUMN_2'],
@@ -302,3 +303,34 @@ def test_add_prefix_to_model_alias():
     )
     test_dict = relation.prep_meta_data()
     assert test_dict["alias"] == "TESTPREFIX_START"
+
+
+def test_add_prefix_to_model_alias_with_no_snowflake_keyword_collision():
+    relation = Relation(
+        'START',
+        ['COLUMN_1', 'COLUMN_2'],
+        'LMS',
+        'models/PROD/LMS',
+        [],
+        [],
+        [],
+        [],
+        prefix="TESTPREFIX"
+    )
+    test_dict = relation.prep_meta_data()
+    assert test_dict["alias"] == "TESTPREFIX_START"
+
+
+def test_snowflake_keyword_collision():
+    relation = Relation(
+        'START',
+        ['COLUMN_1', 'COLUMN_2'],
+        'LMS',
+        'models/PROD/LMS',
+        ['START'],
+        [],
+        [],
+        [],
+    )
+    test_dict = relation.prep_meta_data()
+    assert test_dict["alias"] == "_START"
